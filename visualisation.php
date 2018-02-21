bab84964fe730737438b077ddad8f1d47bc5d317
collotm <32330365+collotm@users.noreply.github.com>
2018-02-21 16:27:07 +0100

    integ. charg. pages

--- visualisation.php
+++ visualisation.php
@@ -18,66 +18,7 @@
 
   <body>
 
-    <header>
-      <div class="collapse bg-dark" id="navbarHeader">
-        <div class="container">
-          <div class="row">
-            <div class="col-sm-8 col-md-7 py-4">
-              <nav class="navbar navbar-expand-md navbar-dark bg-dark fixed-top">
-                <a class="navbar-brand" href="#">Menu</a>
-                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarsExampleDefault" aria-controls="navbarsExampleDefault" aria-expanded="false" aria-label="Toggle navigation">
-                  <span class="navbar-toggler-icon"></span>
-                </button>
-
-                <div class="collapse navbar-collapse" id="navbarsExampleDefault">
-                  <ul class="navbar-nav mr-auto">
-                    <li class="nav-item active">
-                      <a class="nav-link" href="#">Visualisation <span class="sr-only">(current)</span></a>
-                    </li>
-                    <li class="nav-item">
-                      <a class="nav-link" href="#">Historique des bassins</a>
-                    </li>
-                    <li class="nav-item dropdown">
-                      <a class="nav-link dropdown-toggle" href="http://example.com" id="dropdown01" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Autre</a>
-                      <div class="dropdown-menu" aria-labelledby="dropdown01">
-                        <a class="dropdown-item" href="#">Qu'est-ce que OrmeauZen ?</a>
-                        <a class="dropdown-item" href="#">Contact</a>
-                      </div>
-                    </li>
-                    <li class="nav-item dropdown">
-                      <a class="nav-link dropdown-toggle" href="http://example.com" id="dropdown01" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Connecté en tant que</a>
-                      <div class="dropdown-menu" aria-labelledby="dropdown01">
-                        <a class="dropdown-item" href="#">Déconnexion</a>
-                      </div>
-                    </li>
-                  </ul>
-                  <form class="form-inline my-2 my-lg-0">
-                    <input class="form-control mr-sm-2" type="text" placeholder="N° de bassin" aria-label="Search">
-                    <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Chercher</button>
-                  </form>
-                </div>
-              </nav>
-            </div>
-          </div>
-        </div>
-      </div>
-      <div class="navbar navbar-dark bg-dark box-shadow">
-        <div class="container d-flex justify-content-between">
-          <a href="#" class="navbar-brand d-flex align-items-center">
-
-            <strong>OrmeauZen</strong>
-          </a>
-
-          <a>
-          <img src="images/fra.jpg">
-          </a>
-
-          <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarHeader" aria-controls="navbarHeader" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
-          </button>
-        </div>
-      </div>
-    </header>
+<?php require_once("header.php"); ?>
 
     <main role="main">
 
@@ -121,7 +62,7 @@
                         <p></p>
                       </td>
                       <td class="valeur" align="center">
-                        0
+                        #valeur
                       </td>  
                     </tr>
 
@@ -142,7 +83,7 @@
                         <button type="button" class="btn btn-sm btn-outline-secondary">-</button>
                       </td>
                       <td>
-                        <p class="card-text">Réglage débit</p>
+                        <p class="card-text">Régl. débit</p>
                       </td>
                       <td align="center">
                         <button type="button" class="btn btn-sm btn-outline-secondary">+</button>
